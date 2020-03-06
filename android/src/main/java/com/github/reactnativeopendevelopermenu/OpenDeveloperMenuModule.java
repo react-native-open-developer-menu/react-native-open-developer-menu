cf6547b2ccbb832593f1d62eafec9b38e4f5f3e3
alexander-w-o-s <53860987+alexander-w-o-s@users.noreply.github.com>
2020-03-06 19:38:48 +0200

    Android - implement open menu code

--- android/src/main/java/com/github/reactnativeopendevelopermenu/OpenDeveloperMenuModule.java
+++ android/src/main/java/com/github/reactnativeopendevelopermenu/OpenDeveloperMenuModule.java
@@ -3,7 +3,10 @@ package com.github.reactnativeopendevelopermenu;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import android.app.Activity;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
 
 public class OpenDeveloperMenuModule extends ReactContextBaseJavaModule {
 
@@ -20,8 +23,18 @@ public class OpenDeveloperMenuModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void open() {
+        // based on https://github.com/facebook/react-native/blob/2c136192a352ff04c8f2191397a769df844933b1/ReactAndroid/src/main/java/com/facebook/react/ReactActivityDelegate.java#L126
+        final ReactNativeHost rnHost = ((ReactApplication) this.reactContext.getApplicationContext()).getReactNativeHost();
+        if (!rnHost.getUseDeveloperSupport()) {
+            return;
+        }
+        Activity activity = getCurrentActivity();
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                rnHost.getReactInstanceManager().showDevOptionsDialog();
+            }
+        });
     }
 }
